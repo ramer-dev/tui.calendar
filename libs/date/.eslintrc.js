376ad2b17a7f29c726272de54bf292999def96a1
ramer-dev <gml4009@gmail.com>
2026-01-11 17:19:10 +0900

    fix : little change to make fork

--- libs/date/.eslintrc.js
+++ libs/date/.eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     'jest/globals': true,
   },
   plugins: ['prettier', 'jest'],
+  rules:{'prettier/prettier':"warn"},
   extends: ['tui', 'plugin:prettier/recommended', 'plugin:jest/recommended'],
   parserOptions: {
     sourceType: 'module',
